5ea7d20286b08ccd7137272a086f081a307ef528
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-07-08 22:52:02 +0200

    Remove debug code

--- src/main/java/talecraft/client/gui/misc/GuiCopyingWorld.java
+++ src/main/java/talecraft/client/gui/misc/GuiCopyingWorld.java
@@ -6,7 +6,6 @@ public class GuiCopyingWorld extends GuiScreen {
 	private String text;
 	public GuiCopyingWorld(String text) {
 		this.text = text;
-		System.out.println("INITIALIZING GUI!!!!!");
 	}
 	@Override
 	public boolean doesGuiPauseGame() {
@@ -17,7 +16,6 @@ public class GuiCopyingWorld extends GuiScreen {
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		// TODO Auto-generated method stub
-		if(!drawn)System.out.println("DRAWING SCREEEEEEN");
 		drawn=true;
 		drawDefaultBackground();
 		drawCenteredString(fontRenderer, "Moving world directory...", width/2, height/2, 16777215);
